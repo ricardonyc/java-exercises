a07aff8fe9d545932bcd5a8414f8ee2ca92e00fe
ricardonyc <rvardo98@gmail.com>
2022-02-06 21:44:25 -0500

    added palindrome

--- UserInputHw.java
+++ UserInputHw.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 class UserInputHw {
    public static void main(String args[]){
+    Scanner scan = new Scanner(System.in);
 
 
-
-
-       Scanner scan = new Scanner(System.in);
+       // Fibonacci series
        System.out.println("Enter a number: ");
        int userNum = scan.nextInt();
        
@@ -19,8 +18,34 @@ class UserInputHw {
            System.out.print(" " + num3);
            num1 = num2;
            num2 = num3;
-
        }
+
+    
+    // Palindrome
+    System.out.println("Enter a string: ");
+    String userString = scan.nextLine();
+    
+    if(isPalindrome(userString.toLowerCase()))
+        System.out.println("This is a palindrome");
+    else 
+        System.out.println("This is NOT a palindrome");
+
        
+   }
+
+
+   static boolean isPalindrome(String str){
+       int i = 0;
+       int j = str.length() - 1;
+
+       while(i < j){
+           if(str.charAt(i) != str.charAt(j))
+           return false;
+
+           i++;
+           j--;
+       }
+
+       return true;
    }
 }
